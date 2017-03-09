93551913e02098e71bd67f8fc3acfc444b4eb2a5
Rob <robmoore121@gmail.com>
2017-03-09 00:04:42 +0000

    Adding a comment to the seemingly dodgy thing that isnt actually too dodgy

--- Test.hs
+++ Test.hs
@@ -12,6 +12,10 @@ main = do
 tests :: [String]
 tests = [
   makeTest "can represent empty object"
+    -- It doesn't know what value it's trying to extract
+    --   (because this is a test, and is hence contrived),
+    --   so without having the type specified, the compiler
+    --   will get scared and complain.
     (extract "key" (Empty :: JSON String))
     Nothing,
   makeTest "can represent a single string field"
