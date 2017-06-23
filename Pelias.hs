355378b1f268f561771a3fffc578f11f20b9c461
Rob <robmoore121@gmail.com>
2017-06-23 15:21:51 +0100

    made optimisation customisable and optional

--- Pelias.hs
+++ Pelias.hs
@@ -29,30 +29,24 @@ data Value = SValue String | NValue String | BValue Bool | NullValue |
 data JSONOperation = Index Int | Get String
   deriving (Show, Eq)
 
+type Optimiser = ([JSONOperation] -> String -> (String, [JSONOperation]))
+
+defaultOptimiser :: Optimiser
+defaultOptimiser ops json = (json, ops)
+
 extract :: [JSONOperation] -> String -> Maybe Value
-extract ops json = (extractValue remainingOps) $ parse result
+extract = optimisedExtract defaultOptimiser
+
+optimisedExtract :: Optimiser -> [JSONOperation] -> String -> Maybe Value
+optimisedExtract optimiser ops json = (extractValue remainingOps) $ parse result
   where
-    (result, remainingOps) = prepareInput ops json
+    (result, remainingOps) = prepareInput optimiser ops json
 
 removeNewlinesAndTabs :: String -> String
 removeNewlinesAndTabs = concat . (map strip) . lines
 
-prepareInput :: [JSONOperation] -> String -> (String, [JSONOperation])
-prepareInput ops = (optimiseInput ops) . removeNewlinesAndTabs
-
-optimiseInput :: [JSONOperation] -> String -> (String, [JSONOperation])
-optimiseInput (Index i : ops) json = (reform $ (substr start end) splitJSON, ops)
-  where
-    splitJSON        = (split "{") json
-    substr :: Int -> Int -> [a] -> [a]
-    substr s e       = (drop s) . (take e)
-    reform :: [String] -> String
-    reform           = concat . (map (\x -> '{' : x))
-    indexItemRange :: Int -> Int
-    indexItemRange x = (length . takeWhile (< (1 + 2 * x))) bracketMap
-    bracketMap       = scanl (+) 0 $ (map (succ . countStrBalance)) $ (drop 2) $ splitJSON
-    (start, end)     = (indexItemRange i, indexItemRange (i + 1))
-optimiseInput ops json = (json, ops)
+prepareInput :: Optimiser -> [JSONOperation] -> String -> (String, [JSONOperation])
+prepareInput optimiser ops = (optimiser ops) . removeNewlinesAndTabs
 
 countStrBalance :: String -> Int
 countStrBalance = countStrBalance' 1
--- test/APITest.hs
+++ test/APITest.hs
@@ -9,24 +9,20 @@ main = do
   (putStr . concat . appendFailedCount) tests
   putStrLn "ALL DONE"
 
--- Disable optimisations
-parser :: [JSONOperation] -> String -> Maybe Value
-parser ops = (extractValue ops) . parse . removeNewlinesAndTabs
-
 tests :: [String]
 tests = [
 
   makeTest "extract with no operations just parses the whole thing"
-      (parser [] "{\"key\":\"value\"}")
+      (extract [] "{\"key\":\"value\"}")
       (Just $ OValue [("key", SValue "value")]),
   makeTest "get a key from an object"
-      (parser [Get "key"] "{\"key\":\"value\"}")
+      (extract [Get "key"] "{\"key\":\"value\"}")
       (Just $ SValue "value"),
   makeTest "get a value from an array"
-      (parser [Index 0] "[\"key\",\"value\"]")
+      (extract [Index 0] "[\"key\",\"value\"]")
       (Just $ SValue "key"),
   makeTest "apply a get then an index"
-      (parser [Get "key", Index 1] "{\"key\":[\"v0\", \"v1\", \"v2\"]}")
+      (extract [Get "key", Index 1] "{\"key\":[\"v0\", \"v1\", \"v2\"]}")
       (Just $ SValue "v1")
   
         ]
