7c915a37a8f7b4efabcc54e052441c5d7e22a48c
Rob <robmoore121@gmail.com>
2017-06-23 01:39:42 +0100

    Added another test for regression against the newlines issue I had earlier

--- EndToEndTest.hs
+++ EndToEndTest.hs
@@ -5,8 +5,13 @@ import RobUnit
 
 main :: IO ()
 main = do
+  -- File test (Here because it involves IO cancer)
+  json            <- readFile "test.json"
+  (AValue values) <- pure $ parse json
+  len             <- pure $ length values
+
   putStrLn "=========END TO END========="
-  (putStr . concat . appendFailedCount) tests
+  (putStr . concat . appendFailedCount) (tests ++ [makeTest "array of many large objects" len 6])
   putStrLn "ALL DONE"
 
 tests :: [String]
